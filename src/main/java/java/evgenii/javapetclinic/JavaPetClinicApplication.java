f24001b6fa870b13ca392b5bfd502422d50353db
Evgenii Bezdomnikov <evgenii.bezdomnikov@hemmersbach.com>
2018-10-27 18:43:06 +0200

    Initial Project Add

---
+++ src/main/java/java/evgenii/javapetclinic/JavaPetClinicApplication.java
@@ -0,0 +1,12 @@
+package java.evgenii.javapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPetClinicApplication.class, args);
+    }
+}
