863fe835d9c0a79e5f8c99eb2b4acca9d2bb3c95
Evgenii Bezdomnikov <evgenii.bezdomnikov@hemmersbach.com>
2019-01-13 19:26:09 +0100

    Update Map Impl to manage setting of ID Property. closes #22.

--- pet-clinic-web/src/main/java/javaevgenii/javapetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/javaevgenii/javapetclinic/bootstrap/DataLoader.java
@@ -25,14 +25,12 @@ public class DataLoader implements CommandLineRunner {
         Owner owner = new Owner();
         owner.setFirstName("Evgenii");
         owner.setLastName("Bezdomnikov");
-        owner.setId(0L);
 
         ownerService.save(owner);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Evgeniia");
         owner2.setLastName("Pavlova");
-        owner2.setId(1L);
 
         ownerService.save(owner2);
 
@@ -41,14 +39,12 @@ public class DataLoader implements CommandLineRunner {
         Vet vet = new Vet();
         vet.setFirstName("John");
         vet.setLastName("Doe");
-        vet.setId(0L);
 
         vetService.save(vet);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jefrey");
         vet2.setLastName("Way");
-        vet2.setId(1L);
 
         vetService.save(vet2);
 
